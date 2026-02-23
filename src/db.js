d885224574fda2b680b6f1de61e7ba4b5d0abda2
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-02-23 21:18:22 +0100

    Added db file

--- src/db.js
+++ src/db.js
@@ -14,11 +14,8 @@ import {Pool} from "pg"
 
 
 const pool= new Pool({
-    user:process.env.DATABASE_USER,
-    database:process.env.DATABASE_NAME,
-    password:process.env.DATABASE_PASSWORD,
-    host:"localhost",
-    port:5432,
+    connectionString:process.env.DATABASE_URL,
+    ssl:false
 })
 
 
