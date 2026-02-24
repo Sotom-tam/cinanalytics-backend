cd73b1eb2e607d972c95507bf479cbb5b4f23b1a
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-02-24 15:21:29 +0100

    tiny bug

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -123,15 +123,10 @@ export async function sendOtp(req, res, next) {
       req.session.otpPending = true;
       console.log("req session email",req.session.pendingEmail)
       await sendVerificationEmail(user.email);
-
       req.session.save(() => {
       return res.status(200).json({ success: true, message: "OTP sent" });
     });
-      return res.status(200).json({
-          success: true,
-          message: "OTP sent successfully",
-        });
-      })
+})
   } catch (err) {
     next(err);
   }
