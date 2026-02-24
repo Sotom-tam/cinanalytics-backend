91248ec969ce9a94b166a9bfe6f623e9cbbaf559
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-02-24 08:22:55 +0100

    bug in google auth

--- src/app.js
+++ src/app.js
@@ -25,7 +25,10 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie:{maxAge:1000*60*60}
+    cookie:{maxAge:1000*60*60,
+        sameSite:"none",
+        secure:true,
+    }
 }))
 
 
