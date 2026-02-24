60ff324644f44eb558398d97457bb86d1e891437
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-02-24 04:09:33 +0100

    swithc back to resend

--- src/utilis/sendmail.js
+++ src/utilis/sendmail.js
@@ -1,29 +1,59 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS,
-    },
-});
+import { Resend } from 'resend';
 
-export const sendEmail = async ({ to, subject, text, html }) => {
-  try {
-    const info = await transporter.sendMail({
-      from: `Cinanalytics <${process.env.GMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html,
-    });
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    console.log("Email sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("Email error:", error);
-    throw error;
+export async function sendEmail({ to, subject, html, text }) {
+  const { data, error } = await resend.emails.send({
+    from: 'Cinalytics onboarding@resend.dev',
+    to,
+    subject,
+    html: html || `<p>${text}</p>`,
+  });
+
+  if (error) {
+    throw new Error(error.message);
   }
-};
+
+  return data;
+}
+
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     auth: {
+//         user: process.env.GMAIL_USER,
+//         pass: process.env.GMAIL_PASS,
+//     },
+//     pool: false,
+//   maxConnections: 1,
+//   maxMessages: 1,
+
+//   connectionTimeout: 20000,
+//   greetingTimeout: 20000,
+//   socketTimeout: 20000,
+
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// export const sendEmail = async ({ to, subject, text, html }) => {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: `Cinanalytics <${process.env.GMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+
+//     console.log("Email sent:", info.messageId);
+//     return info;
+//   } catch (error) {
+//     console.error("Email error:", error);
+//     throw error;
+//   }
+// };
