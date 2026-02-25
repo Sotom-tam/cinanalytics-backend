2bb667c18353706194ac0e53114fb40f51712da6
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-02-25 16:17:58 +0100

    I forgot to send the project key as the response

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -5,7 +5,7 @@ export async function addNewProjectControl(req,res){
     try {
         const {projectUrl}=req.body
         const result= await addNewProjectServices(projectUrl) 
-        res.status(200).json({message:"Project Saved",success:true})
+        res.status(200).json({message:"Project Saved",projectKey:result.project_key,success:true})
     } catch (error) {
         res.status(500).json({erorr:"error "})
     }
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -6,6 +6,7 @@ import {addNewProjectControl} from "../controllers/projectController.js"
 const router=Router()
 
 router.post("/add-project",addNewProjectControl)
+router.post("/add-project",addNewProjectControl)
 
 
 export default router
