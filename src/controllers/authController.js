96104cb3e2a87a9688a3a6193f9181e1c06faa0f
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-02-26 13:03:25 +0100

    add otp header

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -182,7 +182,7 @@ export const verifyOtp = async (req, res, next) => {
       return res.status(200).json({message: "Otp Verified Successfully",success: true,});
     });
     }else{
-      return res.status(400).json({message:result.message,success:false})
+      return res.status(400).json({header:"Wrong Otp",message:result.message,success:false})
     }
   } catch (error) {
     return res.status(400).json({
