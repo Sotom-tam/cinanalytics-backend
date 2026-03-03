43e4a05dc99778244315625efe622313737865cd
Tamunowari Sotom <142884295+Sotom-tam@users.noreply.github.com>
2026-03-03 10:05:56 +0100

    changed session to last 1 day

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie:{maxAge:1000*60*60,
+    cookie:{maxAge:1000*60*60*24,
         sameSite:isProd?"none":"lax",
         httpOnly:true,
         secure:isProd,
